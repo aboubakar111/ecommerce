aeab46d6ca1b47fd166d3b7a89965cd18c902dc5
aboubakar111 <aboubakarissa111@gmail.com>
2022-09-27 11:40:47 +0300

    Lesson completed 22

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -4,12 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\HomeSlider;
+use App\Models\Product;
 
 class HomeComponent extends Component
 {
     public function render()
     {
         $sliders = HomeSlider::where('status',1)->get();
-        return view('livewire.home-component', ['sliders' => $sliders])->layout('layouts.base');
+        //This is for calling lproducts function
+        $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
+        return view('livewire.home-component', ['sliders' => $sliders,'lproducts'=>$lproducts])->layout('layouts.base');
     }
 }
