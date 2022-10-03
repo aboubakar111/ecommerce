c3328fde5e6c8d3525445a17f45fcf6a27f79349
aboubakar111 <49412051+aboubakar111@users.noreply.github.com>
2022-10-03 17:34:47 +0300

    Revert "Ahmood"

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -5,29 +5,20 @@ namespace App\Http\Livewire;
 use App\Models\Category;
 use App\Models\HomeCategory;
 use Livewire\Component;
-use App\Models\HomeCategory ;
 use App\Models\HomeSlider;
 use App\Models\Product;
-
-
 
 class HomeComponent extends Component
 {
     public function render()
     {
         $sliders = HomeSlider::where('status',1)->get();
-        $lproducts = Product::orderBy('created_at','DESC')->get()->take(8);
+        //This is for calling lproducts function
+        $lproducts = Product::orderBy('created_at', 'DESC')->get()->take(8);
         $category = HomeCategory::find(1);
         $cats = explode(',',$category->sel_categories);
-        $categories = Category::whereIn('id',$cats )->get();
+        $categories = Category::whereIn('id',$cats)->get();
         $no_of_products = $category->no_of_products;
-
-        $sproducts = Product::where('sale_price','>',0)->inRandomOrder()->get()->take(8);
-
-        return view('livewire.home-component', ['sliders'=>$sliders, 
-                                                'lproducts'=>$lproducts,
-                                                'categories'=>$categories, 
-                                                'no_of_products'=> $no_of_products,
-                                                'sproducts'=>$sproducts ])->layout('layouts.base');
+        return view('livewire.home-component', ['sliders' => $sliders,'lproducts'=>$lproducts,'categories'=>$categories,'no_of_products'=>$no_of_products])->layout('layouts.base');
     }
 }
